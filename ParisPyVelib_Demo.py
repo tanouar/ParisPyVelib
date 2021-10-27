620cbf48085ea4635098030fff69ea696069a23e
Hermine Berthon <hermine.berthon@gmail.com>
2021-10-27 14:55:35 +0200

    Update streamlit

--- ParisPyVelib_Demo.py
+++ ParisPyVelib_Demo.py
@@ -35,7 +35,6 @@ st.sidebar.image('Velib.png')
 sommaire = ['Introduction', 'Datasets', 'Evolution Temporelle', 'Prédiction du trafic', 'Conclusion et Perspectives']
 parties = st.sidebar.radio('', sommaire)
 
-
 # Introduction
 
 
@@ -43,8 +42,8 @@ if parties == sommaire[0]:
     st.title('ParisPyVelib')
     st.image('Velib.png')
     st.header('Analyse et prédiction du trafic des vélos de 2018 à 2021 dans la ville de Paris')
-    st.markdown('''Projet réalisé par... en tant que projet fil-rouge de la formation de Data Analyst de DataScientest...
-                ''')
+    st.markdown('''<p style='text-align: justify'>Projet réalisé par Tarik Anouar, Céline Doussot et Hermine Berthon en tant que projet fil-rouge de la formation de Data Analyst de DataScientest.
+                 </p>''',  unsafe_allow_html=True)
     st.markdown('''<p style='text-align: justify'>
                 Un peu partout dans Paris, des totems bleus sont placés le long des grandes artères. Ces bornes, servent à comptabiliser 
                 chaque jour le nombre de cyclistes, par heure à divers endroits de la ville, le plus souvent dans les deux sens de circulation.
@@ -127,8 +126,6 @@ if parties == sommaire[1]:
                     cible est le comptage horaire mais les autres colonnes importantes pour son étude et prédiction, sont les colonnes
                     temporelles ainsi que les coordonnées géographiques.
                     </p>''',  unsafe_allow_html=True)
-        st.header('Distribution')
-        
     
     if data == 'Données météo':
         st.header('Description')
@@ -139,36 +136,59 @@ if parties == sommaire[1]:
                     permettant un accès libre sur toutes les données météos provenant de la ville d'Athis-Mons (91),
                     seul compteur disponible en île-de-France. Cela correspond, après sélection de 2018 à 2021,(même période que pour les compteurs vélib) 
                     à un dataframe de 9944 rows × 82 columns. Celui-ci a ensuite été réduit, par la suite, pour ne sélectionner que les données intéressantes.
-                    </p>''',  unsafe_allow_html=True)
-        st.header('Preprocessing')
-        st.markdown('''<p style='text-align: justify'>
-                    Lorsque nous avons voulu introduire les données météos à notre dataset initial, nous
-nous sommes aperçus que de nombreuses colonnes étaient inutiles ou comportaient beaucoup
-de NANs. De plus, les données météo n’étaient mises à jour que toutes les 3h et non toutes les
-heures comme notre dataset standard. Pour pallier ce problème, nous avons sélectionné
-certaines colonnes pertinentes pour l’étude de la fréquentation vélo comme la pluie, le vent ou
-encore la neige, puis nous avons utilisé un backfill pour compléter les données manquantes sur
-certaines heures.
-                    </p>''',  unsafe_allow_html=True)
-        st.header('Distribution')
+                    </p>''',  unsafe_allow_html=True)            
+        
 
     if data == 'Dataset final':
         st.header('Description')
         st.markdown('''<p style='text-align: justify'>
-                    A ce dataset, nous avons ajoutédes données sur les
+                    Au dataset rassemblant toute les données précédentes, nous avons ajouté des données sur les
                     <a href= "https://www.data.gouv.fr/fr/datasets/vacances-scolaires-par-zones/">
                      vacances scolaires</a>
-                     et sur les
+                     , sur les
                      <a href= "https://www.data.gouv.fr/fr/datasets/jours-feries-en-france/">
                      jours fériés</a> et les confinements.
                     </p>''',  unsafe_allow_html=True)
+                    
+        # Import du df_hour
+        df_hour = pd.read_csv(dataset_2018_2021_donnees_velib_meteo_hour)
+        df_final = df_hour.drop(['Unnamed: 0'], axis = 1)
+        st.text('Affichage des 5 premières lignes du dataset')
+        st.write(df_final.head())
+        
+        st.header('Preprocessing')
+        st.markdown('''<p style='text-align: justify'>
+                    Lorsque nous avons voulu introduire les données météos à notre dataset initial, nous
+                    nous sommes aperçus que de nombreuses colonnes étaient inutiles ou comportaient beaucoup
+                    de NANs. De plus, les données météo n’étaient mises à jour que toutes les 3h et non toutes les
+                    heures comme notre dataset standard. Pour pallier ce problème, nous avons sélectionné
+                    certaines colonnes pertinentes pour l’étude de la fréquentation vélo comme la pluie, le vent ou
+                    encore la neige, puis nous avons utilisé un backfill pour compléter les données manquantes sur
+                    certaines heures.
+                    </p>''',  unsafe_allow_html=True)
+                    
+        st.header('Distribution')
+        Liste_col = ['Count_by_hour', 'Latitude', 'Longitude',
+             'M_Date_Count', 'D_Date_Count', 'Dweek_Date_Count', 'H_Date_Count',
+             'Y_Date_Instal', 'M_Date_Instal', 'D_Date_Instal',
+             'T°C', 'Precip_last3h', 'HR%', 'High_ice','Wind_speed_mean10mn']
 
+        df_final['Latitude'] = df_final['Latitude'].astype('float64')
+        df_final['Longitude'] = df_final['Longitude'].astype('float64')
+        option2 = st.selectbox('Sélectionner la variable', Liste_col) 
+        
+        fig = plt.figure(figsize = (10,4))
+        sns.boxplot(data=df_final, y=Liste_col, x = 'Y_Date_Count')
+        st.pyplot(fig)
+        
+
+        
+        
 # Evolution Temporelle
 if parties == sommaire[2]:
     st.title(parties)
         
-    # Import du df_hour
-    df_hour = pd.read_csv(dataset_2018_2021_donnees_velib_meteo_hour)
+
     # Suppression des col inutiles
     df = df_hour.drop(['Date', 'Date_instal', 'Source', 'Direction',
        'Latitude', 'Longitude', 'Coord', 'Y_Date_Instal',
